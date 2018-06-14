705e725cc0689cfdc5f63767771e82c8da954610
Kinqston <vanek-cs@mail.ru>
2018-06-14 13:03:53 +1000

    door,pip_boy,player_class

---
+++ Assets/Scripts/Pip_Boy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pip_Boy : MonoBehaviour {
+
+	public void Up_Max_Life()
+    {
+        Player.Max_Life++;
+    }
+    public void Up_accuracy()
+    {
+        Player.accuracy++;
+    }
+    public void Up_Damage()
+    {
+        Player.damage++;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public static int Life;
+    public int Startlife;
+    public int StartArmor;
+    public int Start_Max_Life;
+    public double Start_accuracy;
+    public double Start_damage;
+
+    public static int Armor;
+
+    public static bool new_level;
+
+    public static int Max_Life;
+    public static double accuracy;  //точность
+    public static double damage;
+    
+
+    void Start()
+    {
+        Life = Startlife;
+        Armor = StartArmor;
+
+        Max_Life = Start_Max_Life;
+        accuracy = Start_accuracy;
+        damage = Start_damage;
+    }
+}
